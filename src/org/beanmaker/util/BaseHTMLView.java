064e5aec4cdf78e37794d623a6363851fd0b62b3
cpythoud <git@pythoud.net>
2015-01-17 16:43:52 -0700

    Splitted BaseView into BaseView and BaseEditableView.
    BaseEditableView inherits from BaseView.
    
    BaseHtmlView now inherits from BaseEditableView.

--- src/org/beanmaker/util/BaseHTMLView.java
+++ src/org/beanmaker/util/BaseHTMLView.java
@@ -3,16 +3,9 @@ package org.beanmaker.util;
 import javax.servlet.ServletContext;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
-public class BaseHTMLView {
-    final private String resourceBundleName;
-    protected ResourceBundle resourceBundle;
-    protected Locale locale;
-
+public class BaseHTMLView extends BaseEditableView {
     protected List<ErrorMessage> errorMessages = new ArrayList<ErrorMessage>();
 
     protected boolean captchaControl = false;
@@ -23,17 +16,7 @@ public class BaseHTMLView {
     protected ServletContext servletContext = null;
 
     public BaseHTMLView(final String resourceBundleName) {
-        this.resourceBundleName = resourceBundleName;
-        setLocale(Locale.getDefault());
-    }
-
-    public void setLocale(final Locale locale) {
-        this.locale = locale;
-        resourceBundle = ResourceBundle.getBundle(resourceBundleName, locale);
-    }
-
-    public List<ErrorMessage> getErrorMessages() {
-        return Collections.unmodifiableList(errorMessages);
+        super(resourceBundleName);
     }
 
     public void setCaptchaControl(final boolean captchaControl) {
--- src/org/beanmaker/util/BaseView.java
+++ src/org/beanmaker/util/BaseView.java
@@ -1,8 +1,5 @@
 package org.beanmaker.util;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -10,8 +7,6 @@ public class BaseView {
     final private String resourceBundleName;
     protected ResourceBundle resourceBundle;
     protected Locale locale;
-
-    protected List<ErrorMessage> errorMessages = new ArrayList<ErrorMessage>();
 
     public BaseView(final String resourceBundleName) {
         this.resourceBundleName = resourceBundleName;
@@ -21,9 +16,5 @@ public class BaseView {
     public void setLocale(final Locale locale) {
         this.locale = locale;
         resourceBundle = ResourceBundle.getBundle(resourceBundleName, locale);
-    }
-
-    public List<ErrorMessage> getErrorMessages() {
-        return Collections.unmodifiableList(errorMessages);
     }
 }
