699dbf68b1abb27b9b80088db58c38691521e901
cpythoud <git@pythoud.net>
2015-05-24 17:09:25 -0700

    All table titles now have an extra css class (th-<title>).

--- src/org/beanmaker/util/BaseMasterTableView.java
+++ src/org/beanmaker/util/BaseMasterTableView.java
@@ -182,11 +182,20 @@ public abstract class BaseMasterTableView extends BaseView {
     }
 
     protected ThTag getTitleCell(final String name,  final String adhocTitle) {
-        return new ThTag(adhocTitle).cssClass(thTitleCssClass).attribute("data-sort-class", "tb-" + name);
+        return new ThTag(adhocTitle)
+                .cssClass(getTitleCellCssClasses(name))
+                .attribute("data-sort-class", "tb-" + name);
     }
 
     protected ThTag getTitleCell(final String name, final Tag adhocTitle) {
-        return new ThTag().cssClass(thTitleCssClass).attribute("data-sort-class", "tb-" + name).child(adhocTitle);
+        return new ThTag()
+                .cssClass(getTitleCellCssClasses(name))
+                .attribute("data-sort-class", "tb-" + name)
+                .child(adhocTitle);
+    }
+
+    protected String getTitleCellCssClasses(final String name) {
+        return thTitleCssClass + " th-" + name;
     }
 
     protected TrTag getTableLine() {
