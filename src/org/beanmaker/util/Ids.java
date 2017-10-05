d1e896e9f8158d9f721736d8ab328aa0a3ff7762
cpythoud <git@pythoud.net>
2017-10-05 15:57:15 -0700

    Add functions to manage lists of IDs

--- src/org/beanmaker/util/Ids.java
+++ src/org/beanmaker/util/Ids.java
@@ -1,7 +1,12 @@
 package org.beanmaker.util;
 
+import org.dbbeans.util.Strings;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Ids {
@@ -27,11 +32,44 @@ public class Ids {
         return Long.valueOf(getProtectionCodeFromParameterName(parameterName, separatorRegex));
     }
 
-    public static Set<Long> getIdSet(final Collection<DbBeanInterface> beans) {
+    public static <T extends DbBeanInterface> Set<Long> getIdSet(final Collection<T> beans) {
         final Set<Long> set = new HashSet<Long>();
 
         for (DbBeanInterface bean: beans)
             set.add(bean.getId());
+
+        return set;
+    }
+
+    public static <T extends DbBeanInterface> String getAggregatedIdString(
+            final Collection<T> beans,
+            final String separator)
+    {
+        final List<Long> ids = new ArrayList<Long>();
+
+        ids.addAll(getIdSet(beans));
+        Collections.sort(ids);
+
+        return Strings.concatWithSeparator(separator, Strings.asListOfStrings(ids));
+    }
+
+    public static Set<Long> getIdSet(final String aggregatedIds, final String separator, final boolean lenient) {
+        final Set<Long> set = new HashSet<Long>();
+
+        int index = 0;
+        for (String val: aggregatedIds.split(separator)) {
+            ++index;
+            final long id = Strings.getLongVal(val);
+            if (id == 0) {
+                if (!lenient)
+                    throw new IllegalArgumentException("Invalid value for id @pos #" + index + ", val = " + val);
+            } else {
+                set.add(id);
+            }
+        }
+
+        if (set.isEmpty() && !lenient)
+            throw new IllegalArgumentException("No id to extract");
 
         return set;
     }
