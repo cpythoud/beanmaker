ebcbbe156b42887fc1d01c508730fcef28251875
cpythoud <git@pythoud.net>
2019-03-01 16:19:05 -0700

    Replace List by Collection for more flexibility

--- src/org/beanmaker/util/Names.java
+++ src/org/beanmaker/util/Names.java
@@ -1,12 +1,13 @@
 package org.beanmaker.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class Names {
 
-    public static <B extends NamedDbBean> List<String> getList(final List<B> beans) {
+    public static <B extends NamedDbBean> List<String> getList(final Collection<B> beans) {
         final List<String> names = new ArrayList<String>();
 
         for (B bean: beans)
@@ -15,14 +16,14 @@ public class Names {
         return names;
     }
 
-    public static <B extends NamedDbBean> List<String> getSortedList(final List<B> beans) {
+    public static <B extends NamedDbBean> List<String> getSortedList(final Collection<B> beans) {
         final List<String> names = getList(beans);
         Collections.sort(names);
         return names;
     }
 
     public static <B extends MultiLingualNamedDbBean> List<String> getList(
-            final List<B> beans,
+            final Collection<B> beans,
             final DbBeanLanguage dbBeanLanguage)
     {
         final List<String> names = new ArrayList<String>();
@@ -34,7 +35,7 @@ public class Names {
     }
 
     public static <B extends MultiLingualNamedDbBean> List<String> getSortedList(
-            final List<B> beans,
+            final Collection<B> beans,
             final DbBeanLanguage dbBeanLanguage)
     {
         final List<String> names = getList(beans, dbBeanLanguage);
@@ -43,7 +44,7 @@ public class Names {
     }
 
     public static <B extends DbBeanMultilingual> List<String> getListFromMultilingualBeans(
-            final List<B> beans,
+            final Collection<B> beans,
             final DbBeanLanguage dbBeanLanguage)
     {
         final List<String> names = new ArrayList<String>();
@@ -55,7 +56,7 @@ public class Names {
     }
 
     public static <B extends DbBeanMultilingual> List<String> getSortedListFromMultilingualBeans(
-            final List<B> beans,
+            final Collection<B> beans,
             final DbBeanLanguage dbBeanLanguage)
     {
         final List<String> names = getListFromMultilingualBeans(beans, dbBeanLanguage);
