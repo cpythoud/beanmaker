7ffa4407e9ed5965f58730ed7effe85e56dbb5fe
cpythoud <git@pythoud.net>
2020-04-21 19:13:03 -0700

    Add support for sorting named beans according to a Locale collation order

--- src/org/beanmaker/util/Names.java
+++ src/org/beanmaker/util/Names.java
@@ -1,9 +1,13 @@
 package org.beanmaker.util;
 
+import java.text.Collator;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 public class Names {
 
@@ -20,6 +24,26 @@ public class Names {
         final List<String> names = getList(beans);
         Collections.sort(names);
         return names;
+    }
+
+    public static <B extends NamedDbBean> void sortList(final List<B> beans) {
+        Collections.sort(beans, new Comparator<B>() {
+            @Override
+            public int compare(B bean1, B bean2) {
+                return bean1.getDisplayName().compareTo(bean2.getDisplayName());
+            }
+        });
+    }
+
+    public static <B extends NamedDbBean> void sortList(final List<B> beans, Locale locale) {
+        final Collator collator = Collator.getInstance(locale);
+        collator.setStrength(Collator.PRIMARY);
+        Collections.sort(beans, new Comparator<B>() {
+            @Override
+            public int compare(B bean1, B bean2) {
+                return collator.compare(bean1.getDisplayName(), bean2.getDisplayName());
+            }
+        });
     }
 
     public static <B extends MultiLingualNamedDbBean> List<String> getList(
@@ -41,6 +65,20 @@ public class Names {
         final List<String> names = getList(beans, dbBeanLanguage);
         Collections.sort(names);
         return names;
+    }
+
+    public static <B extends MultiLingualNamedDbBean> void sortList(
+            final List<B> beans,
+            final DbBeanLanguage dbBeanLanguage)
+    {
+        final Collator collator = Collator.getInstance(dbBeanLanguage.getLocale());
+        collator.setStrength(Collator.PRIMARY);
+        Collections.sort(beans, new Comparator<B>() {
+            @Override
+            public int compare(B bean1, B bean2) {
+                return collator.compare(bean1.getDisplayName(dbBeanLanguage), bean2.getDisplayName(dbBeanLanguage));
+            }
+        });
     }
 
     public static <B extends DbBeanMultilingual> List<String> getListFromMultilingualBeans(
