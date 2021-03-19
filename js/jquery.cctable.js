e701c5ff9e6e498e43265d53294343c403dc793a
Christophe Pythoud <git@pythoud.net>
2021-03-18 18:28:50 -0700

    Add advanced searching functions to cctable

--- js/jquery.cctable.js
+++ js/jquery.cctable.js
@@ -1,7 +1,7 @@
 ;(function ($) {
 
     function zebra($table, opts) {
-        var index = 0;
+        let index = 0;
         $table.find('tbody tr').each(function () {
             if (!$(this).hasClass(opts.filteredCssClass)) {
                 ++index;
@@ -14,7 +14,7 @@
     }
 
     function setCookie($table, col, value) {
-        var name = "cctable|" + $table.attr('id') + '|' + col;
+        const name = "cctable|" + $table.attr('id') + '|' + col;
         document.cookie = name + "=" + encodeURIComponent(value);
     }
 
@@ -22,15 +22,15 @@
         if (document.cookie === "")
             return;
 
-        var startOfName = "cctable|" + $table.attr('id') + "|";
-        var cookies = document.cookie.split(";");
-        for (var i = 0; i < cookies.length; i++) {
-            var nameValuePair = cookies[i].split("=");
-            var name = $.trim(nameValuePair[0]);
+        const startOfName = "cctable|" + $table.attr('id') + "|";
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const nameValuePair = cookies[i].split("=");
+            const name = $.trim(nameValuePair[0]);
             if (name.indexOf(startOfName) === 0) {
-                var value = decodeURIComponent(nameValuePair[1]);
-                var info = name.split("|");
-                var field = info[2];
+                const value = decodeURIComponent(nameValuePair[1]);
+                const info = name.split("|");
+                const field = info[2];
 
                 $table.find('.' + opts.formElementFilterCssClass + '[name="' + field + '"]').val(value);
                 filter($table, opts);
@@ -41,17 +41,17 @@
     // ------------------------------------------------------------------------
 
     function updateFilteringCounters($table, opts) {
-        var idTable = $table.attr('id');
-        var total = $('#' + idTable + "_total").text();
-        var filteredOut = $table.find('tr.' + opts.filteredCssClass).length;
-        var shown = total - filteredOut;
+        const idTable = $table.attr('id');
+        const total = $('#' + idTable + "_total").text();
+        const filteredOut = $table.find('tr.' + opts.filteredCssClass).length;
+        const shown = total - filteredOut;
 
         $('#' + idTable + "_shown").text(shown);
         $('#' + idTable + "_filtered_out").text(filteredOut);
     }
 
     function removeFiltering($table, opts) {
-        var count = 0;
+        let count = 0;
         $table.find('tr').each(function () {
             $(this).removeClass(opts.filteredCssClass);
             ++count;
@@ -69,13 +69,13 @@
     }
 
     function filter($table, opts) {
-        var didFilter = false;
+        let didFilter = false;
         $table.find('.' + opts.formElementFilterCssClass).each(function () {
-            var filterName = this.name;
-            var filterVal = $.trim($(this).val()).toLowerCase();
+            const filterName = this.name;
+            const filterVal = $.trim($(this).val());  // ! .toLowerCase();
             if (filterVal !== '') {
                 $table.find('td.' + filterName).each(function () {
-                    var content;
+                    let content;
                     if ($(this).data('filter-value'))
                         content = $(this).data('filter-value').toLowerCase();
                     else
@@ -97,12 +97,70 @@
         zebra($table, opts);
     }
 
+    function advancedSearch(opts) {
+        const $form = $('form[name="tb-advanced-search-form"]');
+        const $table = $('#' + $form.find('input[name="tb-table-id"]').val());
+        const filterName = $form.find('input[name="tb-filter-name"]').val();
+        const keywords = $form.find('textarea[name="tb-search-keywords"]').val().trim();
+        const andModality = $form.find('input[name="tb-filter-mods"]:checked').val() === 'AND';
+
+        const keywordList = keywords === '' ? [] : keywords.split(/\s+/);
+
+        // ? All basic search elements removed before advanced search (combination would make no sense)
+        $table.find('.' + opts.formElementFilterCssClass).each(function () {
+            $(this).val('');
+        });
+        removeFiltering($table, opts);
+
+        if (keywordList.length > 0) {
+            if (andModality) {
+                $table.find('td.' + filterName).each(function () {
+                    let content;
+                    if ($(this).data('filter-value'))
+                        content = $(this).data('filter-value').toLowerCase();
+                    else
+                        content = $(this).text().toLowerCase();
+                    let foundAll = true;
+                    for (let i = 0; i < keywordList.length; ++i) {
+                        if (content.indexOf(keywordList[i]) === -1) {
+                            foundAll = false;
+                            break;
+                        }
+                    }
+                    if (!foundAll)
+                        $(this).closest('tr').addClass(opts.filteredCssClass);
+                });
+            } else {
+                $table.find('td.' + filterName).each(function () {
+                    let content;
+                    if ($(this).data('filter-value'))
+                        content = $(this).data('filter-value').toLowerCase();
+                    else
+                        content = $(this).text().toLowerCase();
+                    let missing = true;
+                    for (let i = 0; i < keywordList.length; ++i) {
+                        if (content.indexOf(keywordList[i]) > -1) {
+                            missing = false;
+                            break;
+                        }
+                    }
+                    if (missing)
+                        $(this).closest('tr').addClass(opts.filteredCssClass);
+                });
+            }
+        }
+
+        updateFilteringCounters($table, opts);
+        zebra($table, opts);
+        opts.hideAdvancedSearchModale();
+    }
+
     // ------------------------------------------------------------------------
 
-    var directionHashes = { };
+    const directionHashes = { };
 
     function sort($table, sortColumn, opts) {
-        var directionHash;
+        let directionHash;
         if (directionHashes[$table])
             directionHash = directionHashes[$table];
         else
@@ -111,13 +169,13 @@
         if (!directionHash[sortColumn])
             directionHash[sortColumn] = 'asc';
 
-        var sortVals = [];
-        var tds = { };
+        const sortVals = [];
+        const tds = { };
 
-        var index = 0;
+        let index = 0;
         $table.find('td.' + sortColumn).each(function () {
-            var $td = $(this);
-            var val;
+            const $td = $(this);
+            let val;
             if ($td.data('sort-value'))
                 val = $td.data('sort-value');
             else
@@ -133,10 +191,10 @@
         if (directionHash[sortColumn] === 'desc')
             sortVals.reverse();
 
-        var $content = $table.find('tbody');
+        const $content = $table.find('tbody');
         $content.empty();
-        var length = sortVals.length;
-        for(var i = 0; i < length; ++i) {
+        const length = sortVals.length;
+        for(let i = 0; i < length; ++i) {
             $content.append(tds[sortVals[i]]);
         }
 
@@ -176,9 +234,9 @@
         }
     }
 
-    function toogleTableMaskingStatus($table, opts) {
-        var $showMoreLink = $('#' + $table.attr('id') + '-masking-link-show');
-        var $showLessLink = $('#' + $table.attr('id') + '-masking-link-hide');
+    function toggleTableMaskingStatus($table, opts) {
+        const $showMoreLink = $('#' + $table.attr('id') + '-masking-link-show');
+        const $showLessLink = $('#' + $table.attr('id') + '-masking-link-hide');
 
         if (tableShowingAllData($table, opts)) {
             $table.removeClass(opts.showMoreCssClass);
@@ -203,12 +261,22 @@
     // ------------------------------------------------------------------------
 
     $.fn.cctable = function(options) {
-        var opts = $.extend({ }, $.fn.cctable.defaults, options);
+        const opts = $.extend({ }, $.fn.cctable.defaults, options);
+        const $body = $('body');
 
         return this.each(function () {
-            var $table = $(this);
+            const $table = $(this);
             $table.find('input.' + opts.formElementFilterCssClass).keyup(function() {
                 filter($table, opts);
+            });
+
+            $table.find('input.' + opts.formElementFilterCssClass).dblclick(function() {
+                opts.showAdvancedSearchModale($table.attr('id'), $(this).attr('name'));
+            });
+
+            $body.on('submit', 'form[name="' + opts.advancedSearchFormName + '"]', function (event) {
+                event.preventDefault();
+                advancedSearch(opts);
             });
 
             $table.find('select.' + opts.formElementFilterCssClass).change(function() {
@@ -233,7 +301,7 @@
 
                 $table.find('a.' + opts.maskingLinkCssClass).on('click', function (event) {
                     event.preventDefault();
-                    toogleTableMaskingStatus($table, opts);
+                    toggleTableMaskingStatus($table, opts);
                     showOrHideColumns($table, opts);
                 });
             }
@@ -246,15 +314,15 @@
                 update: function (event, ui) {
                     zebra($table, opts);
 
-                    var $item = ui.item;
+                    const $item = ui.item;
 
-                    var $prevItem = $item.prev();
+                    const $prevItem = $item.prev();
                     if ($prevItem.is('tr') && !$prevItem.hasClass(opts.filteredCssClass)) {
                         opts.moveAfterFunction($item.attr('id'), $prevItem.attr('id'));
                         return;
                     }
 
-                    var $nextItem = $item.next();
+                    const $nextItem = $item.next();
                     if ($nextItem.is('tr') && !$nextItem.hasClass(opts.filteredCssClass)) {
                         opts.moveBeforeFunction($item.attr('id'), $nextItem.attr('id'));
                         return;
@@ -294,6 +362,13 @@
         moveBeforeFunction: function (itemId, moveBeforeItemId) {
             console.log('Moved element ID = ' + itemId);
             console.log('To be moved before element with ID = ' + moveBeforeItemId);
+        },
+        advancedSearchFormName: 'tb-advanced-search-form',
+        showAdvancedSearchModale: function (idTable, filterName) {
+            console.log("SHOW ADVANCED SEARCH MODALE: tb ID = " + idTable + ", name = " + filterName);
+        },
+        hideAdvancedSearchModale: function () {
+            console.log("HIDE ADVANCED SEARCH MODALE");
         },
         zebraCssClass: 'alternate'
     };
